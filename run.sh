2b6ff270005616e8d2d13ba99d98b728792cd41b
Avery lee <1840123105@qq.com>
2022-07-15 02:32:28 +0800

    fi 11

---
+++ run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+touch Dockerfile
+#获取文件的位置
+filename="Dockerfile"
+inputText='FROM alpine 
+CMD ["echo","Hello world"]'
+
+if [ -f "$filename" ]
+then
+    echo "$inputText" >> "$filename"
+fi
+
+1
